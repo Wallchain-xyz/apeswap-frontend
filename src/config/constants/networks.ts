3ca6025077ddacb9b2894fcc197b158b0a8686e5
Harambe-Nakamoto <haramben@protonmail.com>
2023-02-14 14:54:47 -0600

    chore: Update smart router version and change network

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -9,6 +9,6 @@ export const RPC_URLS: Record<SupportedChainId, string[]> = {
   [SupportedChainId.BSC_TESTNET]: ['https://data-seed-prebsc-2-s3.binance.org:8545/'],
   [SupportedChainId.POLYGON]: ['https://polygon-mainnet.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847'],
   [SupportedChainId.POLYGON_MUMBAI]: ['https://matic-mumbai.chainstacklabs.com'],
-  [SupportedChainId.MAINNET]: ['https://mainnet.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847'],
+  [SupportedChainId.MAINNET]: ['https://mainnet.infura.io/v3/b3852de923e0444b93e0c8892ab8ef39'],
   [SupportedChainId.TLOS]: ['https://mainnet.telos.net/evm'],
 }
