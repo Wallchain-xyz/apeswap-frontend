1e358ee5808b421f3e9fd5883068e0f665627a38
Harambe-Nakamoto <haramben@protonmail.com>
2023-02-22 18:37:24 -0600

    chore: Swap polygon rpc

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -7,7 +7,7 @@ export const RPC_URLS: Record<SupportedChainId, string[]> = {
     'https://bsc-dataseed1.defibit.io',
   ],
   [SupportedChainId.BSC_TESTNET]: ['https://data-seed-prebsc-2-s3.binance.org:8545/'],
-  [SupportedChainId.POLYGON]: ['https://polygon-mainnet.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847'],
+  [SupportedChainId.POLYGON]: ['https://nd-612-180-351.p2pify.com/47c8945a9c47a940f53ebab964a2c585'],
   [SupportedChainId.POLYGON_MUMBAI]: ['https://matic-mumbai.chainstacklabs.com'],
   [SupportedChainId.MAINNET]: ['https://mainnet.infura.io/v3/b3852de923e0444b93e0c8892ab8ef39'],
   [SupportedChainId.TLOS]: ['https://mainnet.telos.net/evm'],
