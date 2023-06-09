ba1db3d7a7d66bb7cf149f1c9043248ac6607bc7
Obie Dobo <obiedobo@gmail.com>
2023-06-09 09:00:13 -0500

    chore: Add beta tag

--- src/components/uikit/Tag/types.ts
+++ src/components/uikit/Tag/types.ts
@@ -29,9 +29,10 @@ export const listTagVariants = {
   HOT: 'hot',
   AUTO: 'auto',
   MAX: 'max',
-  LIQUIDITY: "liquidity",
-  RESERVE: "reserve",
-  LAUNCH: "launch",
+  BETA: 'beta',
+  LIQUIDITY: 'liquidity',
+  RESERVE: 'reserve',
+  LAUNCH: 'launch',
 } as const
 
 export type ListTagVariants = (typeof listTagVariants)[keyof typeof listTagVariants]
--- src/views/LHD/components/TitleCards/index.tsx
+++ src/views/LHD/components/TitleCards/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Flex, Link, Text } from 'components/uikit'
+import { Flex, Link, ListTag, Text } from 'components/uikit'
 import StatCard from './StatCard'
 import { useTranslation } from 'contexts/Localization'
 import { styles } from './styles'
@@ -13,41 +12,51 @@ const TitleCards = () => {
       <Flex sx={styles.titleContainer}>
         <Flex sx={{ width: '100%' }}>
           <Text sx={styles.titleText}>{t('Liquidity Health Dashboard')}</Text>
+          <ListTag variant="beta" />
         </Flex>
         <Flex sx={{ width: '100%', mt: '10px' }}>
           <Text sx={styles.detailText}>
-            {t('ApeSwap’s data visualization tool that provides insights into the liquidity levels and sustainability of cryptocurrency projects.')}
+            {t(
+              'ApeSwap’s data visualization tool that provides insights into the liquidity levels and sustainability of cryptocurrency projects.',
+            )}
           </Text>
         </Flex>
         <Flex sx={{ width: '100%', mt: '10px' }}>
-          <Link href='https://apeswap.gitbook.io/apeswap-finance/welcome/master'>
-            <Text sx={styles.btnText}>
-              {t('Learn More')}
-            </Text>
+          <Link href="https://apeswap.gitbook.io/apeswap-finance/welcome/master">
+            <Text sx={styles.btnText}>{t('Learn More')}</Text>
           </Link>
-          <Link href='https://github.com/ApeSwapFinance/lhd-config'>
-            <Text
-              sx={styles.btnText}>
-              {t('Submit data update')}
-            </Text>
+          <Link href="https://github.com/ApeSwapFinance/lhd-config">
+            <Text sx={styles.btnText}>{t('Submit data update')}</Text>
           </Link>
         </Flex>
       </Flex>
       <Flex sx={styles.cardsContainer}>
         <StatCard
-          title='Industry Average'
+          title="Industry Average"
           value={(parseFloat(averageTotalScore) * 100).toFixed()}
           footerInfo={<>{`${averageChange ?? '0'}% in last 7 days`}</>}
         />
         <StatCard
-          title='Chains Supported'
+          title="Chains Supported"
           value={chainsSupported}
-          footerInfo={<Link href='https://apeswap.gitbook.io/apeswap-finance/welcome/master' target='_blank' sx={{ color: 'yellow' }}>See which chains</Link>}
+          footerInfo={
+            <Link
+              href="https://apeswap.gitbook.io/apeswap-finance/welcome/master"
+              target="_blank"
+              sx={{ color: 'yellow' }}
+            >
+              See which chains
+            </Link>
+          }
         />
         <StatCard
-          title='Supported Tokens'
+          title="Supported Tokens"
           value={tokensTracked.toString()}
-          footerInfo={<Link href='https://github.com/ApeSwapFinance/lhd-config' target='_blank' sx={{ color: 'yellow' }}>Verify Your Project?</Link>}
+          footerInfo={
+            <Link href="https://github.com/ApeSwapFinance/lhd-config" target="_blank" sx={{ color: 'yellow' }}>
+              Verify Your Project?
+            </Link>
+          }
         />
       </Flex>
     </Flex>
