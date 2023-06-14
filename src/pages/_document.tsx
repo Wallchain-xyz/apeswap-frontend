6e1ea9cb5c7e46b3ec651f939483be7e5f3daebd
Cryptoims <cryptoimss@gmail.com>
2023-06-14 13:40:01 -0300

    chore: Fixes andmeta test 12

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,8 +24,9 @@ class MyDocument extends Document {
             content="ApeSwap is a multichain DeFi Hub offering an accessible, transparent, and secure experience for everyone."
           />
           <meta name="theme-color" content="#000000" />
-          {/* <meta name="twitter:image" content="https://apeswap.finance/twitter.png" /> */}
-          <meta name="twitter:image" content="https://i.imgur.com/g1sFeUS.png" />
+          <meta name="twitter:image" content="https://apeswap.finance/twitter.png" />
+          {/* <meta name="twitter:image" content="https://i.imgur.com/g1sFeUS.png" />
+          <meta name="og:image" content="https://i.imgur.com/g1sFeUS.png" /> */}
           <meta
             name="twitter:description"
             content="Swap, stake, and earn cryptocurrencies, all in one place. Accessible, transparent, and secure for everyone."
