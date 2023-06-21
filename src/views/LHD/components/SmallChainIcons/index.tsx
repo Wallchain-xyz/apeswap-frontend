2bae07cb2799d362ec45c1c0a69f39cfd213db39
Obie Dobo <obiedobo@gmail.com>
2023-06-21 18:10:36 -0500

    chore: Add chains and remove manual entry

--- src/views/LHD/components/SmallChainIcons/index.tsx
+++ src/views/LHD/components/SmallChainIcons/index.tsx
@@ -1,31 +1,9 @@
+import { CHAIN_DETAILS } from 'views/LHD/utils/config'
 import Icon from './Icon'
 
-export const icons: { [key: string]: JSX.Element } = {
-  '1': <Icon chain={'1'} />, //eth
-  '56': <Icon chain={'56'} />, //bsc
-  '137': <Icon chain={'137'} />, //matic
-  '40': <Icon chain={'40'} />, //telos
-  '42161': <Icon chain={'42161'} />, //arbitrum
-  '250': <Icon chain={'250'} />, //fantom
-  '43114': <Icon chain={'43114'} />, //avalanche
-  '128': <Icon chain={'128'} />, //heco
-  '10': <Icon chain={'10'} />, //optimism
-  '25': <Icon chain={'25'} />, // cronos
-  '1285': <Icon chain={'1285'} />, // moonriver
-  '1284': <Icon chain={'1284'} />, // moonbeam
-  '1088': <Icon chain={'1088'} />, // metis
-  '1666600000': <Icon chain={'1666600000'} color="black" />, // harmony
-  '42220': <Icon chain={'42220'} />, // celo
-  '288': <Icon chain={'288'} color="black" />, // boba
-  '100': <Icon chain={'100'} />, // xdai
-  '324': <Icon chain={'324'}></Icon>, // zksync
-  '592': <Icon chain={'592'}></Icon>, // astar
-  '106': <Icon chain={'106'}></Icon>, // velas
-  '321': <Icon chain={'321'}></Icon>, // kcc
-  '10000': <Icon chain={'10000'}></Icon>, // smart bch
-  '42170': <Icon chain={'42170'}></Icon>, // arbitrum nova
-  '1116': <Icon chain={'1116'}></Icon>, // core
-  '2222': <Icon chain={'2222'}></Icon>, // Kava
-  '4689': <Icon chain={'4689'}></Icon>, // IoTeX
-  '1313161554': <Icon chain={'1313161554'} />, // aurora
-}
+export const icons = CHAIN_DETAILS.reduce((obj, chainDetail) => {
+  return {
+    ...obj,
+    [chainDetail.chainId]: <Icon chain={chainDetail.chainId} />,
+  }
+}, {})
