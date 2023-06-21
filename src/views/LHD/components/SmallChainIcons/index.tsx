b246276ade1a738e8cc7c911e0205e6af9d330ec
Obie Dobo <obiedobo@gmail.com>
2023-06-21 18:21:11 -0500

    chore: Resolve build error

--- src/views/LHD/components/SmallChainIcons/index.tsx
+++ src/views/LHD/components/SmallChainIcons/index.tsx
@@ -1,7 +1,7 @@
 import { CHAIN_DETAILS } from 'views/LHD/utils/config'
 import Icon from './Icon'
 
-export const icons = CHAIN_DETAILS.reduce((obj, chainDetail) => {
+export const icons: any = CHAIN_DETAILS.reduce((obj, chainDetail) => {
   return {
     ...obj,
     [chainDetail.chainId]: <Icon chain={chainDetail.chainId} />,
