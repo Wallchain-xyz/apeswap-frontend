6488108644973b9c7a20bb9e1b4c8d3e3e86274a
scott@prof-s.xyz <scott@prof-s.xyz>
2023-06-21 23:40:59 -0700

    Test image share forced url

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -51,7 +51,7 @@ const PageContainer = ({
   //   address
   // }`
 
-  const imageURL = 'https://res.cloudinary.com/dswmrqgwy/image/upload/v1/lhd-share-images/lhd_share_56_0x78f5d389f5cdccfc41594abab4b0ed02f31398b3'
+  const imageURL = 'https://res.cloudinary.com/dswmrqgwy/image/upload/v1/lhd-share-images/lhd_share_42161_0xd77b108d4f6cefaa0cae9506a934e825becca46e'
 
   console.log(imageURL)
 
