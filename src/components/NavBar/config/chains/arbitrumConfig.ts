8fd4d40aeb2fc47dc2da96b548c1f39c7afde3cf
Obie Dobo <obiedobo@gmail.com>
2023-07-13 15:49:44 -0500

    chore: Navbar changes

--- src/components/NavBar/config/chains/arbitrumConfig.ts
+++ src/components/NavBar/config/chains/arbitrumConfig.ts
@@ -1,67 +1,58 @@
 const TEMP_APESWAP_URL = process.env.NEXT_PUBLIC_LEGACY_APESWAP_URL
+
 const arbitrumConfig = [
   {
     label: 'Exchange',
-    lightIcon: 'ExchangeLightImage',
-    darkIcon: 'ExchangeDarkImage',
     items: [
       {
         label: 'Swap',
         href: 'https://dex.apeswap.finance/swap',
-        isNew: false,
       },
       {
         label: 'Liquidity',
         href: 'https://dex.apeswap.finance/zap',
-        isNew: false,
       },
       {
         label: 'Pro Trading',
         href: 'https://pro.apeswap.finance',
-        isNew: false,
       },
     ],
   },
   {
-    label: 'Raise',
-    items: [
-      {
-        label: 'Bonds',
-        href: `/bonds`,
-      },
-      {
-        label: 'Liquidity Health',
-        href: `/liquidity-health`,
-      },
-    ],
+    label: 'Bonds',
+    href: '/bonds',
   },
   {
-    label: 'Explore',
+    label: 'Liquidity Health',
+    href: `/liquidity-health`,
+  },
+  {
+    label: 'More',
     items: [
       {
         label: 'ApeStats',
         href: `${TEMP_APESWAP_URL}/apestats`,
       },
       {
-        label: 'Dashboard',
+        label: 'Protocol Data',
         href: `/protocol-dashboard`,
-      },
-      {
-        label: 'Documentation',
-        href: 'https://apeswap.gitbook.io/apeswap-finance/',
       },
       {
         label: 'Charts',
         href: `${TEMP_APESWAP_URL}/info`,
       },
       {
+        label: 'Lend',
+        href: 'https://lending.apeswap.finance/',
+      },
+      {
+        label: 'ApeSwap NFTs',
+        href: '/nft',
+      },
+      {
         label: 'Governance',
         href: 'https://discuss.apeswap.finance',
       },
-      // {
-      //   label: 'Newsletter',
-      //   href: '?modal=newsletter',
-      // },
     ],
   },
 ]
