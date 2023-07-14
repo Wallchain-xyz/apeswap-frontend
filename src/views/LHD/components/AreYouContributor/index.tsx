d7b50cf4085bfbf72c2a096db1bb3532301fce42
Obie Dobo <obiedobo@gmail.com>
2023-07-14 17:39:14 -0500

    chore: LHD lint & link

--- src/views/LHD/components/AreYouContributor/index.tsx
+++ src/views/LHD/components/AreYouContributor/index.tsx
@@ -7,18 +7,29 @@ const Index = () => {
   const { t } = useTranslation()
   return (
     <Flex sx={styles.contributorCont}>
-      <Text sx={styles.title}>
-        {t('Are you a contributor to this project?')}
-      </Text>
+      <Text sx={styles.title}>{t('Are you a contributor to this project?')}</Text>
       <Text sx={styles.info}>
-        {t('Visit our documentation to learn more about how Liquidity Scores are calculated. Or, submit updated liquidity data through GitHub.')}
+        {t(
+          'Visit our documentation to learn more about how Liquidity Scores are calculated. Or, submit updated liquidity data through GitHub.',
+        )}
       </Text>
       <Flex sx={styles.btnCont}>
-        <Button onClick={() => window.open('https://apeswap.gitbook.io/apeswap-finance/welcome/master', '_blank')} variant='secondary'
-                sx={{ ...styles.btn, m: ['0 0 10px 0', '0 0 10px 0', '0 0 10px 0', '0 20px 0 0'] }}>
+        <Button
+          onClick={() =>
+            window.open(
+              'https://apeswap.gitbook.io/apeswap-finance/product-and-features/liquidity-health-dashboard',
+              '_blank',
+            )
+          }
+          variant="secondary"
+          sx={{ ...styles.btn, m: ['0 0 10px 0', '0 0 10px 0', '0 0 10px 0', '0 20px 0 0'] }}
+        >
           {t('LEARN MORE')}
         </Button>
-        <Button onClick={() => window.open('https://github.com/ApeSwapFinance/lhd-config', '_blank')} sx={{ ...styles.btn, width: ['100%', '100%', '100%', '215px'] }}>
+        <Button
+          onClick={() => window.open('https://github.com/ApeSwapFinance/lhd-config', '_blank')}
+          sx={{ ...styles.btn, width: ['100%', '100%', '100%', '215px'] }}
+        >
           {t('SUBMIT DATA UPDATE')}
         </Button>
       </Flex>
