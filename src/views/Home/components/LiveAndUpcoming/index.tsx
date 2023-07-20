56f77286aadc6d142d435a219ea3313d0576a042
ApeLoko <apeloko.dev@gmail.com>
2023-07-20 19:13:08 +0700

    migrated NewsModal

--- src/views/Home/components/LiveAndUpcoming/index.tsx
+++ src/views/Home/components/LiveAndUpcoming/index.tsx
@@ -1,15 +1,23 @@
 import { Box } from 'theme-ui'
 
 // Components
-import { Text, Flex } from 'components/uikit'
+import { Text, Flex, Button } from 'components/uikit'
+import NewsModal from 'components/NewsLetter/NewsModal'
+
+// Constants
+import { mailChimpUrl } from 'config/constants/api'
 
 // Hooks
 import { useTranslation } from 'contexts/Localization'
 import useGetLiveAndUpcoming from 'hooks/queries/useGetLiveAndUpcoming'
+import useModal from 'hooks/useModal'
 
 const LiveAndUpcoming = () => {
   const { data } = useGetLiveAndUpcoming()
   const { t } = useTranslation()
+
+  const [onPresentModal] = useModal(<NewsModal mailChimpUrl={mailChimpUrl} />, false, false, 'newsModal')
+
   console.log({ data })
   return (
     <Flex
@@ -22,17 +30,20 @@ const LiveAndUpcoming = () => {
       }}
     >
       <Text sx={{ fontSize: ['25px', '25px', '35px'], fontWeight: '500' }}>{t('Live & Upcoming')}</Text>
-      <Text
+      <Button
+        onClick={onPresentModal}
+        variant="text"
         sx={{
           fontSize: ['12px', '12px', '15px'],
           fontWeight: '500',
           color: '#FFB300',
           opacity: '0.5',
-          mt: ['4px', '4px', '8px'],
+          textTransform: 'initial',
+          pl: '0px',
         }}
       >
         {t('Subscribe to our newsletter >')}
-      </Text>
+      </Button>
     </Flex>
   )
 }
