83c271c67485f6a7464b2d685841d514f40c02c7
ApeLoko <apeloko.dev@gmail.com>
2023-07-21 02:01:15 +0700

    added light theme icons

--- src/views/Home/components/FriendsOfApeSwap/index.tsx
+++ src/views/Home/components/FriendsOfApeSwap/index.tsx
@@ -1,4 +1,5 @@
-import { Grid, Box } from 'theme-ui'
+import Image from 'next/image'
+import { Grid, Box, useThemeUI } from 'theme-ui'
 
 // Components
 import { Flex, Text } from 'components/uikit'
@@ -7,9 +8,25 @@ import { Flex, Text } from 'components/uikit'
 import { useTranslation } from 'contexts/Localization'
 
 const FriendsOfApeSwap = () => {
+  const { colorMode } = useThemeUI()
   const { t } = useTranslation()
 
-  const friends = new Array(10).fill(1)
+  const friends = [
+    'nfty',
+    'mogul',
+    'ceek',
+    'animoca',
+    'brave',
+    'apollox',
+    'mogul',
+    'moonpay',
+    'chainlink',
+    'telos',
+    'gauntlet',
+    'mogul',
+  ]
+
+  const isDark = true ? 'light' : 'dark'
 
   return (
     <Flex
@@ -19,42 +36,24 @@ const FriendsOfApeSwap = () => {
         flexDirection: 'column',
         alignSelf: 'center',
         mt: ['62px', '62px', '90px'],
+        mb: ['55px', '55px', '130px'],
       }}
     >
       <Flex sx={{ mb: ['10px', '10px', '35px'], justifyContent: 'center' }}>
         <Text sx={{ fontSize: ['25px', '25px', '35px'], fontWeight: '500' }}>{t('Friends of ApeSwap')}</Text>
       </Flex>
-      {/* <Grid
-        sx={{
-          gridTemplateColumns: 'repeat(auto-fill, minmax(179px, 1fr))',
-        }}
-      >
-        {friends.map((_, index) => (
-          <Box key={index} sx={{ bg: 'salmon' }}>
-            {index}
-          </Box>
-        ))}
-      </Grid> */}
 
       <Grid
         sx={{
-          gridTemplateColumns: ['1fr 1fr', '1fr 1fr', '1fr 1fr 1fr 1fr 1fr'],
+          gridTemplateColumns: ['1fr 1fr', '1fr 1fr', '1fr 1fr 1fr', '1fr 1fr 1fr 1fr 1fr', '1fr 1fr 1fr 1fr 1fr 1fr'],
         }}
       >
-        {friends.map((_, index) => (
-          <Box key={index} sx={{ bg: 'salmon', height: '71px' }}>
-            {index}
+        {friends.map((friend, index) => (
+          <Box key={index} sx={{ height: '71px', maxWidth: '179px', position: 'relative' }}>
+            <Image src={`/images/apeswap-friends/${friend}-${colorMode}.svg`} fill alt="friend logo" key={index} />
           </Box>
         ))}
       </Grid>
-
-      {/* <Flex sx={{ flexWrap: 'wrap', justifyContent: 'start', gap: '20px' }}>
-        {friends.map((_, index) => (
-          <Box key={index} sx={{ bg: 'salmon', width: '180px' }}>
-            {index}
-          </Box>
-        ))}
-      </Flex> */}
     </Flex>
   )
 }
