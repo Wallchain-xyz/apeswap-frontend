5781dfbf97c4b2077470d16a697e5d3e0cb3c4d1
ApeLoko <apeloko.dev@gmail.com>
2023-07-24 16:05:08 +0700

    added link, fixed minor UI bugs

--- src/components/NavBarNew/components/DropdownMenu.tsx
+++ src/components/NavBarNew/components/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Box } from 'theme-ui'
 
 // Components
@@ -19,17 +20,16 @@ interface DropdownMenuProps {
 const DropdownMenu = ({ isVisible, items }: DropdownMenuProps) => {
   const [isHovered, setIsHovered] = useState<number | null>(null)
 
-  console.log({ items })
-
   const { t } = useTranslation()
   return (
     <Flex
       sx={{
         display: isVisible ? 'flex' : 'none',
         position: 'absolute',
-        top: '36px',
+        top: '35px',
         left: '0',
         bg: 'rgba(33, 33, 33, 0.90)',
+        width: 'max-content',
         px: '30px',
         py: '20px',
         borderRadius: 'normal',
@@ -37,50 +37,51 @@ const DropdownMenu = ({ isVisible, items }: DropdownMenuProps) => {
       }}
     >
       <Flex sx={{ gap: '10px', flexDirection: 'column' }}>
-        {items.map(({ itemLabel, itemDesc, icon }, index) => {
+        {items.map(({ itemLabel, itemDesc, icon, href }, index) => {
           const isItemHovered = isHovered === index
           return (
-            <Flex
-              onMouseEnter={() => setIsHovered(index)}
-              onFocus={() => setIsHovered(index)}
-              onMouseLeave={() => setIsHovered(null)}
-              key={itemLabel}
-              sx={{
-                p: '10px',
-                gap: '15px',
-                opacity: '0.5',
-                borderRadius: 'normal',
-                alignItems: 'center',
-                width: 'max-content',
-                minWidth: '250px',
-                height: '59px',
-                '&:hover': { bg: 'white1', opacity: '0.9' },
-              }}
-            >
-              <Box sx={{ height: '25px', width: '25px', position: 'relative' }}>
-                <Image src={icon} alt={`${itemLabel} logo`} fill />
-              </Box>
-              <Flex sx={{ flexDirection: 'column' }}>
-                <Text sx={{ fontSize: '16px', fontWeight: '500' }}>{t(itemLabel)}</Text>
-                <Text sx={{ fontSize: '12px', fontWeight: '500', color: 'buttonDisabledText' }}>{t(itemDesc)}</Text>
-              </Flex>
+            <Link href={href} key={itemLabel} style={{ color: 'inherit', textDecoration: 'inherit' }}>
               <Flex
+                onMouseEnter={() => setIsHovered(index)}
+                onFocus={() => setIsHovered(index)}
+                onMouseLeave={() => setIsHovered(null)}
                 sx={{
-                  visibility: isItemHovered ? 'inherit' : 'hidden',
+                  p: '10px',
+                  gap: '15px',
+                  opacity: '0.5',
+                  borderRadius: 'normal',
                   alignItems: 'center',
-                  justifyContent: 'center',
-                  marginLeft: 'auto',
-                  height: '15px',
-                  width: '15px',
-                  borderRadius: '20px',
-                  bg: 'white1',
-                  fontSize: '12px',
-                  lineHeight: '12px',
+                  width: 'auto',
+                  minWidth: '250px',
+                  height: '59px',
+                  '&:hover': { bg: 'white3', opacity: '0.9' },
                 }}
               >
-                {'>'}
+                <Box sx={{ height: '25px', width: '25px', position: 'relative' }}>
+                  <Image src={icon} alt={`${itemLabel} logo`} fill />
+                </Box>
+                <Flex sx={{ flexDirection: 'column' }}>
+                  <Text sx={{ fontSize: '16px', fontWeight: '500' }}>{t(itemLabel)}</Text>
+                  <Text sx={{ fontSize: '12px', fontWeight: '500', color: 'buttonDisabledText' }}>{t(itemDesc)}</Text>
+                </Flex>
+                <Flex
+                  sx={{
+                    visibility: isItemHovered ? 'inherit' : 'hidden',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginLeft: 'auto',
+                    height: '15px',
+                    width: '15px',
+                    borderRadius: '20px',
+                    bg: 'rgba(253, 251, 245, 0.20)',
+                    fontSize: '12px',
+                    lineHeight: '12px',
+                  }}
+                >
+                  {'>'}
+                </Flex>
               </Flex>
-            </Flex>
+            </Link>
           )
         })}
       </Flex>
